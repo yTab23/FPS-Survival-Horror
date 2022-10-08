c9604381ec95ef1be2448210dcc9688dc89c603b
Yuki Tabata <ytabata11@gmail.com>
2022-10-08 21:06:29 +1100

    Created Pickup message.

---
+++ Assets/My Scripts/Pickups.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour
+{
+    RaycastHit hit;
+    [SerializeField] float Distance = 4.0f;
+    [SerializeField] GameObject PickupMessage;
+
+    private float RayDistance;
+    private bool CanSeePickup = false;
+
+    void Start()
+    {
+        PickupMessage.gameObject.SetActive(false);
+        RayDistance = Distance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, out hit, RayDistance))
+        {
+            if(hit.transform.tag == "Apple")
+            {
+                CanSeePickup = true;
+            }
+            else
+            {
+                CanSeePickup = false;
+            }
+        }
+
+        if(CanSeePickup == true)
+        {
+            PickupMessage.gameObject.SetActive(true);
+            RayDistance = 1000f;
+        }
+
+        if(CanSeePickup == false)
+        {
+            PickupMessage.gameObject.SetActive(false);
+            RayDistance = Distance;
+        }
+    }
+}
