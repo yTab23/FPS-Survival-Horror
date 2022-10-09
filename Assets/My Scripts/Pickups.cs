f97377af11e1baf91c64d6ea5eb97b1649663124
Yuki Tabata <ytabata11@gmail.com>
2022-10-09 20:01:35 +1100

    Removed Pickup ability

--- Assets/My Scripts/Pickups.cs
+++ Assets/My Scripts/Pickups.cs
@@ -22,7 +22,7 @@ public class Pickups : MonoBehaviour
     {
         if(Physics.Raycast(transform.position, transform.forward, out hit, RayDistance))
         {
-            if(hit.transform.tag == "Apple")
+            if(hit.transform.tag == "Apple" && SaveScript.Apples < 6)
             {
                 CanSeePickup = true;
                 if(Input.GetKeyDown(KeyCode.E))
@@ -31,10 +31,10 @@ public class Pickups : MonoBehaviour
                     SaveScript.Apples += 1;
                 }
             }
-            else if(hit.transform.tag == "Battery")
+            else if(hit.transform.tag == "Battery" && SaveScript.Batteries < 4)
             {
                 CanSeePickup = true;
-                if(Input.GetKeyDown(KeyCode.E))
+                if(Input.GetKeyDown(KeyCode.E) )
                 {
                     Destroy(hit.transform.gameObject);
                     SaveScript.Batteries += 1;
